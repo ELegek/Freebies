c9baf529c8a9913bb0594e3be2b99f1ca496e4a6
Evgeny Lezhepekov <evgeny.legek@yandex.ru>
2020-07-21 20:26:36 +0300

    Header

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,23 @@
-$(function(){
+const selectElement = document.querySelector("[data-select]");
 
+selectElement.addEventListener("click", function (event) {
 
+    if (event.target.hasAttribute("data-select-item")) {
+        const itemTitle = event.target.getAttribute("data-select-item");
 
-  
+        event.target
+            .closest("[data-select]")
+            .querySelector("[data-select-title]")
+            .textContent = itemTitle;
+        event.target
+            .closest("[data-select]")
+            .querySelector(".header__select-dropdown")
+            .classList.toggle("hidden");
+
+    } else {
+        this
+            .querySelector('.header__select-dropdown')
+            .classList.toggle("hidden");
+    }
+
 });
